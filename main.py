d04a5aa021d053eb4e7fad7a22823f684c292310
tommangione <tommangione@pm.me>
2025-04-07 19:15:32 -0400

    main.py counts words in hard coded file

---
+++ main.py
@@ -0,0 +1,17 @@
+def get_book_text():
+    with open("books/frankenstein.txt") as f:
+        return f.read()
+
+def word_counter():
+    text = get_book_text()
+    word_list = text.split()
+    counter = 0
+    for word in word_list:
+        counter+=1
+    print(f"{counter} words found in the document")
+
+def main():
+    get_book_text()
+    word_counter()
+
+main()
