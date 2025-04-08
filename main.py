ef7dbfdcbeb358f174710af3b865faa04cb60299
tommangione <tommangione@pm.me>
2025-04-08 07:57:23 -0400

    added funcs to stats.py

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-def get_book_text():
-    with open("books/frankenstein.txt") as f:
-        return f.read()
-
-def word_counter():
-    text = get_book_text()
-    word_list = text.split()
-    counter = 0
-    for word in word_list:
-        counter+=1
-    print(f"{counter} words found in the document")
+from stats import get_book_text
+from stats import word_counter
 
 def main():
     get_book_text()
