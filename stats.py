5449e8e83ff0dc912e3c68ca19990ba2e64d6ecd
tommangione <tommangione@pm.me>
2025-04-08 08:42:52 -0400

    began work on character counter using a dictionary

--- stats.py
+++ stats.py
@@ -9,3 +9,36 @@ def word_counter():
     for word in word_list:
         counter+=1
     print(f"{counter} words found in the document")
+
+def char_counter():
+    counter_dict = {
+        'a' : 0,
+        'b' : 0,
+        'c' : 0,
+        'd' : 0,
+        'e' : 0,
+        'f' : 0,
+        'g' : 0,
+        'h' : 0,
+        'i' : 0,
+        'j' : 0,
+        'k' : 0,
+        'l' : 0,
+        'm' : 0,
+        'n' : 0,
+        'o' : 0,
+        'p' : 0,
+        'q' : 0,
+        'r' : 0,
+        's' : 0,
+        't' : 0,
+        'u' : 0,
+        'v' : 0,
+        'w' : 0,
+        'x' : 0,
+        'y' : 0,
+        'z' : 0,
+    }
+    text = get_book_text().lower()
+    
+    pass
